b99ba2c95ef009d79343b210d96a84734ecb0a41
Rafael Freitas <vidarafael@hotmail.com>
2021-11-13 15:27:56 -0300

    initial responsive

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const allLinksFromSidebar = document.querySelectorAll('.change-content')
 const allContent = document.querySelectorAll('.main-content')
+const sidebarGeralActive = document.querySelector(".sibebar-theme-geral")
+const sidebarGamesActive = document.querySelector(".sibebar-theme-jogos")
 
 console.log(allContent[0])
 
 allLinksFromSidebar[0].classList.add("ativo")
 allContent[0].classList.add("ativo")
+sidebarGeralActive.classList.add("ativo")
 
 allLinksFromSidebar.forEach((link, index) => {
   link.addEventListener('click', (event) => {
+    if(index <= 1) {
+      sidebarGeralActive.classList.add("ativo")
+      sidebarGamesActive.classList.remove("ativo")
+    } else {
+      sidebarGamesActive.classList.add("ativo")
+      sidebarGeralActive.classList.remove("ativo")
+    }
     allLinksFromSidebar.forEach((element) => {
       element.classList.remove("ativo")
     })
